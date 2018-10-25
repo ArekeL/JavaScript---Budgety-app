db79000eda82bdfa15693376779d034a9bc9817d
Arek <39218511+ArekeL@users.noreply.github.com>
2018-10-25 18:35:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var budgetController = (function() {
+    var x = 23;
+
+    var add = function(a) {
+      return x + a;
+    }
+
+    return {
+      publicTest: function(b) {
+        console.log(add(b));
+      }
+    }
+})();
