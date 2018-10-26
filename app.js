d05a3d3326285bde3eee50ce0898dc50ec95971b
Arek <39218511+ArekeL@users.noreply.github.com>
2018-10-26 05:39:30 +0200

    BUDGET CONTROLER #2

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
 var budgetController = (function() {
 
   // some code
-    
+
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
   // some code
 }() );
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    console.log('Button was clicked');
 
+
+  });
+
+  document.addEventListener('keypress', function(event){
+    console.log(event);
+  });
 
 } )(budgetController, UIController);
