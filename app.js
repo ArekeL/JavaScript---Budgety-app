6c4abd0f5f016b67574122f11878258a46b21132
Arek <39218511+ArekeL@users.noreply.github.com>
2018-10-28 08:01:02 +0100

    addEventListener/key and btn
    Lots of problems, less of code.

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-// BUDGET CONTROLLER
-var budgetController = (function() {
-
-  // some code
-
-})();
+// BUDGET CONTROLER
+var budgetController = (function(){
+  //Some code
+});
 
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function(){
 
-  // some code
-}() );
+// Some code
 
-// GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log('Button was clicked');
+});
+
+// GLOBAL CONTROLLER
+var controller = (function(budgetCTRL, UICTrl){
+
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+
+    // 2. Add the item to the bidget CONTROLLER
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the
+    console.log('It works');
+  };
 
 
-  });
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
-    console.log(event);
+
+     if(event.keyCode === 13 || event.which === 13) {
+       ctrlAddItem();
+     }
+
   });
 
-} )(budgetController, UIController);
+
+
+
+
+
+
+})(budgetController, UIController);
