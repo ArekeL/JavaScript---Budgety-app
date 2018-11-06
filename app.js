38cec92454c6de0cbb99b3b4f1b90f8b209c5783
Arek <39218511+ArekeL@users.noreply.github.com>
2018-11-06 07:01:27 +0100

    Just before work with DOM

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ var budgetController = (function(){
     },
 
     testing: function() {
-
       console.log(data);
 
     }
@@ -79,15 +78,28 @@ var UIController = (function(){
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+    addListItem: function(obj, type) {
+      var html, newHtml;
+        // Create HTML string which placeholder data
+        if (type === 'inc') {
+            html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item_ value">%value%</div>  <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+        } else if (type === 'exp') {
+          html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+        }
+
+        // Replace the placeholder text wich some actual data
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', obj.value);
+//}
+        // INSERT THE html into the DOM
+
+
+    },
 
     getDOMstrings: function() {
       return DOMstrings;
-    },
-
-    testing: function() {
-      console.log(data);
     }
-
   };
 
 })();
