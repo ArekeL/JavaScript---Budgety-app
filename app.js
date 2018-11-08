f6b8bba29bb4e8f61875ed01e00e99fd492421ba
Arek <39218511+ArekeL@users.noreply.github.com>
2018-11-08 06:44:18 +0100

    Some bug

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ var budgetController = (function(){
     },
 
     testing: function() {
-
       console.log(data);
 
     }
@@ -68,7 +67,9 @@ var UIController = (function(){
       inputType: '.add__type',
       inputDescription: '.add__description',
       inputValue: '.add__value',
-      inputBtn: '.add__btn'
+      inputBtn: '.add__btn',
+      incomeContainer: '.income__list',
+      expensesContainer: '.expenses__list'
   };
 
   return {
@@ -79,21 +80,40 @@ var UIController = (function(){
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+        // Create HTML string which placeholder data
+        if (type === 'inc') {
+
+          element = DOMstrings.incomeContainer;
+
+          html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item_ value">%value%</div>  <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+        } else if (type === 'exp') {
+
+          element = DOMstrings.expensesContainer;
+
+          html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+        }
+
+        // Replace the placeholder text wich some actual data
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', obj.value);
+//}
+        // INSERT THE html into the DOM
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
 
     getDOMstrings: function() {
       return DOMstrings;
-    },
-
-    testing: function() {
-      console.log(data);
     }
-
   };
 
 })();
 
 // GLOBAL CONTROLLER
-var controller = (function(budgetCTRL, UICtrl){
+var controller = (function(budgetCtrl, UICtrl){
 
   var setupEventListeners = function () {
 
@@ -122,7 +142,7 @@ var controller = (function(budgetCTRL, UICtrl){
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
-
+    UICtrl.addListItem(newItem, input.type);
     // 4. Calculate the budget
 
     // 5. Display the budget on the
